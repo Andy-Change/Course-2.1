5c19c29c867e5e9bf886ded807c03c667564d2ae
Andy-Change <remy.ilab@gmail.com>
2020-07-05 16:38:58 +0300

    Initial Commit

---
+++ Course 2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Course 2.1
+//
+//  Created by admin on 05.07.2020.
+//  Copyright © 2020 ILAB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
